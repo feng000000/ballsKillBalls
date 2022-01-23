737b249bc25a61c155219885509a6e9fdcaea542
feng ling <flfeng0@outlook.com>
2022-01-24 02:41:27 +0800

    fix bug

--- thegame/urls/index.py
+++ thegame/urls/index.py
@@ -4,6 +4,6 @@ from thegame.views.index import index
 urlpatterns = [
     path("", index, name="index"),
     path("menu/", include("thegame.urls.menu.index")),
-    path("playground/", index("thegame.urls.playground.index")),
+    path("playground/", include("thegame.urls.playground.index")),
     path("settings/", include("thegame.urls.settings.index")),
 ]
